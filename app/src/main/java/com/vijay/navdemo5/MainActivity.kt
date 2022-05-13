a71adc57d1c7b2b7e6f3a85872545098ab679521
vijayakumarksv <svijayakumarbecse777@gmail.com>
2022-05-13 22:01:43 +0530

    initial commit

---
+++ app/src/main/java/com/vijay/navdemo5/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.vijay.navdemo5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.vijay.navdemo5.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+    }
+}
